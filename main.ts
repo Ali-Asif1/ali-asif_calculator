99ab7645fef0bc445ff515b685421861249cb73a
Muhammad Asif <159746725+Ali-Asif1@users.noreply.github.com>
2024-03-23 17:09:11 +0500

    first project

---
+++ main.ts
@@ -0,0 +1,42 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+const answer = await inquirer.prompt([
+  { type: "number", name: "firstNum", message: chalk.greenBright("Enter your first number: ")},
+  { type: "number", name: "secondNum", message:chalk.greenBright("Enter your second number: ")},
+  {
+    type: "list",
+    name: "operator",
+    message: chalk.greenBright("Select operator:"),
+    choices: ["+", "-", "*", "/"],
+  },
+]);
+if (answer.operator == "+") {
+  console.log(chalk.yellow(
+    `${answer.firstNum} ${answer.operator} ${answer.secondNum} = ${
+      answer.firstNum + answer.secondNum
+    }`)
+  );
+} else if (answer.operator == "-") {
+  console.log(chalk.yellow(
+    `${answer.firstNum} ${answer.operator} ${answer.secondNum} = ${
+      answer.firstNum - answer.secondNum
+    }`)
+  );
+} else if (answer.operator == "*") {
+  console.log(chalk.yellow(
+    `${answer.firstNum} ${answer.operator} ${answer.secondNum} = ${
+      answer.firstNum * answer.secondNum
+    }`)
+  );
+} else if (answer.operator == "/") {
+  console.log(chalk.yellow(
+    `${answer.firstNum} ${answer.operator} ${answer.secondNum} = ${
+      answer.firstNum / answer.secondNum
+    }`)
+  );
+} else {
+  console.log(chalk.red("Please select valid operator"));
+}
